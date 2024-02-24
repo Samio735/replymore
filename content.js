75182cb2c27a53862ed4d9a7e325722a0159426e
Samy <s_rahim@estin.dz>
2024-02-24 01:33:45 +0100

    fixed reset stats

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-let inserted = false;
+// let inserted = false;
 // to do if implemented : change color on darkmode and lightmode
 // function updatePostCountUI(postCount) {
 //   const followersEL = document.querySelector(
@@ -43,47 +43,106 @@ let inserted = false;
 //   inserted = true;
 // }
 
-let postCount = chrome.storage.sync.get("postCount", (data) => {
-  postCount = data.postCount || 0;
-});
+function createContainer() {
+  const container = document.createElement("div");
+  container.id = "stats-container";
+  container.style.position = "fixed";
+  container.style.top = "0";
+  container.style.right = "0";
+  container.style.padding = "10px";
+  container.style.backgroundColor = "rgb(29, 155, 240)";
+  container.style.color = "#ffffff";
+  container.style.fontFamily = "Arial, sans-serif";
+  container.style.fontSize = "14px";
+  container.style.fontWeight = "bold";
+  container.style.zIndex = "9999";
+  container.style.borderBottomLeftRadius = "10px";
+  container.style.display = "flex";
+  container.style.flexDirection = "Column";
+  container.style.alignItems = "center";
 
-let todayCount = chrome.storage.sync.get("todayCount", (data) => {
-  todayCount = data.todayCount || 0;
-});
+  document.body.prepend(container);
+}
 
 function createStatsElement() {
   const statsElement = document.createElement("div");
   statsElement.id = "post-stats";
-  statsElement.style.position = "fixed";
-  statsElement.style.top = "0";
-  statsElement.style.right = "0";
-  statsElement.style.padding = "10px";
-  statsElement.style.backgroundColor = "rgb(29, 155, 240)";
-  statsElement.style.color = "#ffffff";
-  statsElement.style.fontFamily = "Arial, sans-serif";
-  statsElement.style.fontSize = "14px";
-  statsElement.style.fontWeight = "bold";
-  statsElement.style.zIndex = "9999";
   statsElement.style.display = "flex";
   statsElement.style.alignItems = "center";
+  statsElement.style.justifyContent = "center";
   statsElement.style.gap = "5px";
-  statsElement.style.borderBottomLeftRadius = "10px";
 
-  document.body.prepend(statsElement);
+  document.querySelector("#stats-container").appendChild(statsElement);
+
+  updateStatsElement();
+}
+
+function createProgressBar() {
+  const progressBarContainer = document.createElement("div");
+  progressBarContainer.id = "post-progress-bar-container";
+  progressBarContainer.style.height = "3px";
+  progressBarContainer.style.width = "100%";
+  progressBarContainer.style.backgroundColor = " #106aa6";
+  progressBarContainer.style.zIndex = "9999";
+  progressBarContainer.style.margin = "0 10px";
+  progressBarContainer.style.marginBottom = "5px";
+  progressBarContainer.style.borderRadius = "20px";
+
+  const progressBar = document.createElement("div");
+  progressBar.id = "post-progress-bar";
+  progressBar.style.width = "4px";
+  progressBar.style.height = "100%";
+  progressBar.style.backgroundColor = "#ffffff";
+  progressBar.style.borderRadius = "20px";
+
+  progressBarContainer.prepend(progressBar);
+  document.querySelector("#stats-container").prepend(progressBarContainer);
+
+  updateProgressBar();
 }
 
 function updateStatsElement() {
-  chrome.storage.sync.get(["postCount", "countDays", "todayCount"], (data) => {
-    const statsElement = document.getElementById("post-stats");
-    const dailyAverage = (data.postCount / data.countDays).toFixed(2);
-    statsElement.innerHTML =
-      // Replace with actual daily average of posts and comments
-      `<span><svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1qtyvf0 r-o4n3w5" style="color: rgb(255, 255, 255); margin-right:2px;"><g><path d="M23 3c-6.62-.1-10.38 2.421-13.05 6.03C7.29 12.61 6 17.331 6 22h2c0-1.007.07-2.012.19-3H12c4.1 0 7.48-3.082 7.94-7.054C22.79 10.147 23.17 6.359 23 3zm-7 8h-1.5v2H16c.63-.016 1.2-.08 1.72-.188C16.95 15.24 14.68 17 12 17H8.55c.57-2.512 1.57-4.851 3-6.78 2.16-2.912 5.29-4.911 9.45-5.187C20.95 8.079 19.9 11 16 11zM4 9V6H1V4h3V1h2v3h3v2H6v3H4z"></path></g></svg></span>  Today : ${data.todayCount}   | Avg : ${dailyAverage} / day `;
-  });
-}
+  chrome.storage.sync.get(
+    ["postCount", "countDays", "todayCount", "dailyGoal"],
+    (data) => {
+      const statsElement = document.getElementById("post-stats");
+      if (data.postCount == undefined) data.postCount = 0;
+      if (data.countDays == undefined) data.countDays = 1;
+      if (data.todayCount == undefined) data.todayCount = 0;
+      if (data.todayCount == undefined) data.todayCount = 0;
+      if (data.todayCount == undefined) data.todayCount = 0;
 
-async function requestPlaySound() {
-  const response = await chrome.runtime.sendMessage({ action: "playAudio" });
+      const dailyAverage = (data.postCount / data.countDays).toFixed(2);
+      statsElement.innerHTML =
+        // Replace with actual daily average of posts and comments
+        `<span><svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1qtyvf0 r-o4n3w5" style="color: rgb(255, 255, 255); margin-right:2px;"><g><path d="M23 3c-6.62-.1-10.38 2.421-13.05 6.03C7.29 12.61 6 17.331 6 22h2c0-1.007.07-2.012.19-3H12c4.1 0 7.48-3.082 7.94-7.054C22.79 10.147 23.17 6.359 23 3zm-7 8h-1.5v2H16c.63-.016 1.2-.08 1.72-.188C16.95 15.24 14.68 17 12 17H8.55c.57-2.512 1.57-4.851 3-6.78 2.16-2.912 5.29-4.911 9.45-5.187C20.95 8.079 19.9 11 16 11zM4 9V6H1V4h3V1h2v3h3v2H6v3H4z"></path></g></svg></span>  
+      Today : ${data.todayCount}${
+          data.dailyGoal > 0 ? `/${data.dailyGoal}` : ""
+        }    | Avg : ${dailyAverage} / day `;
+    }
+  );
+}
+function updateProgressBar() {
+  chrome.storage.sync.get(["postCount", "todayCount", "dailyGoal"], (data) => {
+    if (!data.dailyGoal) {
+      const progressBarContainer = document.getElementById(
+        "post-progress-bar-container"
+      );
+      progressBarContainer.style.display = "none";
+      console.log("hidden");
+      return;
+    }
+
+    const progressBarContainer = document.getElementById(
+      "post-progress-bar-container"
+    );
+    console.log("not hidden");
+
+    progressBarContainer.style.display = "block";
+    const progressBar = document.getElementById("post-progress-bar");
+    const progress = (data.postCount / data.dailyGoal) * 100;
+    progressBar.style.width = `${progress}%`;
+  });
 }
 
 // Add event listener for detecting comment or post activity
@@ -102,48 +161,51 @@ document.addEventListener("click", function (event) {
       todayCount++;
       chrome.storage.sync.set({ postCount: postCount, todayCount: todayCount });
       updateStatsElement();
+      updateProgressBar();
       // updatePostCountUI(postCount);
     });
   }
 });
 
-let userId;
-chrome.storage.sync.get("userId", (data) => {
-  userId = data.userId;
-  console.log(userId);
-});
-console.log(userId);
+// let userId;
+// chrome.storage.sync.get("userId", (data) => {
+//   userId = data.userId;
+//   console.log(userId);
+// });
+// console.log(userId);
 
-// check if the day has changed
-const today = new Date().toLocaleDateString();
-chrome.storage.sync.get(["currentDay", "countDays"], (data) => {
-  (!data.countDays || data.countDays == 0) && (data.countDays = 1);
-  if (data.currentDay !== today) {
-    chrome.storage.sync.set({
-      currentDay: today,
-      todayCount: 0,
-      countDays: data.countDays + 1,
-    });
-  }
-});
+function checkDateChange() {
+  const today = new Date().toLocaleDateString();
+  chrome.storage.sync.get(["currentDay", "countDays"], (data) => {
+    (!data.countDays || data.countDays == 0) && (data.countDays = 1);
+    if (data.currentDay !== today) {
+      chrome.storage.sync.set({
+        currentDay: today,
+        todayCount: 0,
+        countDays: data.countDays + 1,
+      });
+    }
+  });
+}
 
-chrome.storage.sync.get("postCount", (data) => {
-  let postCount = data.postCount || 0;
+function init() {
+  checkDateChange();
+  createContainer();
   createStatsElement();
-  updateStatsElement(postCount);
-});
+  createProgressBar();
+}
 
 // Select the followers element and update the text
 
-setTimeout(() => {
-  setInterval(() => {
-    chrome.storage.sync.get("postCount", (data) => {
-      let postCount = data.postCount;
-      if (postCount == undefined) postCount = 0;
-      // updatePostCountUI(postCount);
-    });
-  }, 100);
-}, 200);
+// setTimeout(() => {
+//   setInterval(() => {
+//     chrome.storage.sync.get("postCount", (data) => {
+//       let postCount = data.postCount;
+//       if (postCount == undefined) postCount = 0;
+//       // updatePostCountUI(postCount);
+//     });
+//   }, 100);
+// }, 200);
 
 const audio = new Audio(chrome.runtime.getURL("Ding.mp3"));
 
@@ -152,17 +214,20 @@ function playAudio() {
   audio.play();
 }
 
+function updateStatsUI() {
+  updateProgressBar();
+  updateStatsElement();
+}
 // update stats and post count on storage change
 chrome.storage.onChanged.addListener((changes, namespace) => {
-  if (changes.postCount) {
-    updateStatsElement();
-    // updatePostCountUI(changes.postCount.newValue);
-  }
+  updateStatsUI();
 });
 
 // update user id on storage change
-chrome.storage.onChanged.addListener((changes, namespace) => {
-  if (changes.userId) {
-    userId = changes.userId.newValue;
-  }
-});
+// chrome.storage.onChanged.addListener((changes, namespace) => {
+//   if (changes.userId) {
+//     userId = changes.userId.newValue;
+//   }
+// });
+
+init();
--- popup.js
+++ popup.js
@@ -1,13 +1,26 @@
-const SaveBtn = document.getElementById("save");
+// const SaveBtn = document.getElementById("save");
 const ResetBtn = document.getElementById("reset");
-const UserIdBtn = document.getElementById("user_id");
+// const UserIdEl = document.getElementById("user_id");
 
-SaveBtn.addEventListener("click", () => {
-  const userId = UserIdBtn.value;
-  console.log(userId);
-  chrome.storage.sync.set({ userId: userId });
-});
+const dailyGoalEl = document.getElementById("daily-goal");
+
+// SaveBtn.addEventListener("click", () => {
+//   const userId = UserIdEl.value;
+//   console.log(userId);
+//   chrome.storage.sync.set({ userId: userId });
+// });
 
 ResetBtn.addEventListener("click", () => {
   chrome.storage.sync.set({ todayCount: 0, postCount: 0, countDays: 1 });
+});
+
+dailyGoalEl.addEventListener("change", (e) => {
+  chrome.storage.sync.set({
+    dailyGoal: Number(e.target.value),
+  });
+});
+
+chrome.storage.sync.get("dailyGoal", (data) => {
+  const dailyGoal = data.dailyGoal || 0;
+  dailyGoalEl.value = dailyGoal;
 });
